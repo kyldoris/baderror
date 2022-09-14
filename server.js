774084565018d67082f416268eb8646823999e09
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-09-14 10:44:42 -0700

    ingredienLines display

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const methodOverride = require("method-override")
 require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
-const searchBarController = require("./controllers/searchBar.index.js")
 
 
 // DATABASE CONFIG
@@ -16,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 // app.use <- MIDDLEWARE diercting a certain traffic to a specific controller 
 app.use(methodOverride("_method"))
-app.use("/searchBar", searchBarController)
+
 
 
 
@@ -31,6 +30,7 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(`${err.message} is mongod not running?`));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo is disconnected "));
+
 
 // LISTENER
 app.listen(3000, () =>{
